79a71eafbdaee513ab00ed2b2b2379694429e94a
Nadhir <mamokaze@gmail.com>
2020-12-05 23:15:44 +0100

    Improvements for the main page

--- public/main.js
+++ public/main.js
@@ -1,16 +1,47 @@
 headerText = document.getElementById('header-text');
 headerText.innerHTML = "";
-let mainText = "Hi, I'm Nadhir.", i = 0;
-let interval = setInterval(() => {
+document.getElementById('title-text').innerHTML = "";
+document.getElementById('content-text').innerHTML = "";
+
+mainText = "Hi, I'm Nadhir.";
+devText = "I'm a Developer";
+contText = "I make stuf bla bla bla idk i use a lot of things idk how to express myself haha lol sorry. Lorem ipsum that latin thing idk haha filler goes brrrrr";
+alphabet = "abcdefghijklmnopqrstuvwxyz";
+i = 0;
+interval = 100;
+sInterval = setInterval(() => {
     headerText.innerHTML += mainText[i];
     i++;
-    if (i==mainText.length) {
-        clearInterval(interval);
+    if (i == mainText.length) {
+        clearInterval(sInterval);
         setTimeout(() => {
-            document.getElementById('header-text').style.transform = 'translateY(-450%)';
+            headerText.style.transform = 'translateY(-800%)';
             setTimeout(() => {
+                headerText.style.opacity = 0;
                 document.getElementsByClassName('App-hidden')[0].style.opacity = 1;
-            }, 300);
+                headerText.parentNode.removeChild(headerText);
+                printLetterByLetter("title-text", devText, 200);
+                printLetterByLetter("content-text", contText, 50);
+            }, 400);
+        }, 1000);
+    } else if (Math.random() < .9) {
+        setTimeout(() => {
+            headerText.innerHTML += alphabet[Math.floor(Math.random() * alphabet.length)];
+        }, 500);
+        setTimeout(() => {
+            headerText.innerHTML = headerText.innerHTML.slice(0, -1);
         }, 500);
     }
-}, 100);
+}, interval);
+
+
+function printLetterByLetter(destination, message, interval){
+    var i = 0;
+    var sInterval = setInterval(function(){
+        document.getElementById(destination).innerHTML += message.charAt(i);
+        i++;
+        if (i > message.length){
+            clearInterval(sInterval);
+        }
+    }, interval);
+}
--- src/App.js
+++ src/App.js
@@ -1,33 +1,32 @@
 import './App.css';
 import Dot from './components/Dot';
-import {ReactComponent as JS} from './components/icons/js.svg';
-import {ReactComponent as HTML} from './components/icons/html.svg';
-import {ReactComponent as CSS} from './components/icons/css.svg';
-import {ReactComponent as MYSQL} from './components/icons/mysql.svg';
-import {ReactComponent as PYTHON} from './components/icons/python.svg';
-import {ReactComponent as JAVA} from './components/icons/java.svg';
-import {ReactComponent as C_LANG} from './components/icons/c.svg';
-import {ReactComponent as NODEJS} from './components/icons/nodejs.svg';
-import {ReactComponent as EXPRESS} from './components/icons/express.svg';
-import {ReactComponent as MONGODB} from './components/icons/mongodb.svg';
-import {ReactComponent as JQUERY} from './components/icons/jquery.svg';
-import {ReactComponent as BOOTSTRAP} from './components/icons/bootstrap.svg';
-import {ReactComponent as REACT} from './components/icons/react.svg';
-import {ReactComponent as GIT} from './components/icons/git.svg';
+import { ReactComponent as JS } from './components/icons/js.svg';
+import { ReactComponent as HTML } from './components/icons/html.svg';
+import { ReactComponent as CSS } from './components/icons/css.svg';
+import { ReactComponent as MYSQL } from './components/icons/mysql.svg';
+import { ReactComponent as PYTHON } from './components/icons/python.svg';
+import { ReactComponent as JAVA } from './components/icons/java.svg';
+import { ReactComponent as C_LANG } from './components/icons/c.svg';
+import { ReactComponent as NODEJS } from './components/icons/nodejs.svg';
+import { ReactComponent as EXPRESS } from './components/icons/express.svg';
+import { ReactComponent as MONGODB } from './components/icons/mongodb.svg';
+import { ReactComponent as JQUERY } from './components/icons/jquery.svg';
+import { ReactComponent as BOOTSTRAP } from './components/icons/bootstrap.svg';
+import { ReactComponent as REACT } from './components/icons/react.svg';
+import { ReactComponent as GIT } from './components/icons/git.svg';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-            <h1 id="header-text">-</h1>
-            <div className="App-hidden">
-                <div className="intro-wrapper">
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1 id="header-text">-</h1>
+                <div className="intro-wrapper App-hidden">
                     <div className="nav-wrapper">
                         <div className="dots-wrapper"><Dot color="#FC6058" /><Dot color="#FEC02F" /><Dot color="#2ACA3E" /></div>
                     </div>
                     <div className="left-column">
-                        <h1>I'm a Developer</h1>
-                        <p>I make stuf bla bla bla idk i use a lot of things idk how to express myself haha lol sorry.<br/>Lorem ipsum that latin thing idk haha filler goes brrrrr</p>
+                        <h1 id="title-text">-</h1>
+                        <p id="content-text">-</p>
                     </div>
                     <div className="right-column">
                         <h4>Languages</h4>
@@ -40,7 +39,7 @@ function App() {
                         <C_LANG width={iconSize} height={iconSize} title="C" />
                         <h4>Libraries / Tools</h4>
                         <NODEJS width={iconSize} height={iconSize} title="Node.js" />
-                        <EXPRESS style={{fill:"#fff"}} width={iconSize} height={iconSize} title="Express.js" />
+                        <EXPRESS style={{ fill: "#fff" }} width={iconSize} height={iconSize} title="Express.js" />
                         <MONGODB width={iconSize} height={iconSize} title="MongoDB" />
                         <JQUERY width={iconSize} height={iconSize} title="jQuery" />
                         <BOOTSTRAP width={iconSize} height={iconSize} title="Bootstrap" />
@@ -48,10 +47,9 @@ function App() {
                         <GIT width={iconSize} height={iconSize} title="Git" />
                     </div>
                 </div>
-            </div>
-      </header>
-    </div>
-  );
+            </header>
+        </div>
+    );
 }
 
 const iconSize = "50px";
