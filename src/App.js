5503d06d4b6c929088864e5f9a5ee0d9170e7a90
Nadhir <mamokaze@gmail.com>
2020-12-05 01:19:32 +0100

    Fix

--- src/App.js
+++ src/App.js
@@ -20,14 +20,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-            <div className="App-head-text">
-                <h1 id="header-text">-</h1>
-            </div>
+            <h1 id="header-text">-</h1>
             <div className="App-hidden">
                 <div className="intro-wrapper">
                     <div className="nav-wrapper">
                         <div className="dots-wrapper"><Dot color="#FC6058" /><Dot color="#FEC02F" /><Dot color="#2ACA3E" /></div>
-                        <div className="contact-nav"><a href="mailto:nadhir02@gmail.com">contact</a></div>
                     </div>
                     <div className="left-column">
                         <h1>I'm a Developer</h1>
